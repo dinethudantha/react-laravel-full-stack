494cc37b2843a5d9fc07e5309c763335317389ad
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-09 19:47:10 +0530

    feature/react-router-dom
    I create react router for Signup , Login and Users pages.

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+import Login from './views/Login.jsx'
+import Signup from './views/Signup.jsx'
+import Users from './views/Users.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <Login />
+  } , 
+  {
+    path: '/login',
+    element: <Login />
+  } , 
+  {
+    path : '/signup',
+    element: <Signup />
+  },
+   {
+    path : '/users',
+    element: <Users />
+  }
+
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
