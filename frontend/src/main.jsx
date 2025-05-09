eb5bd6c3d2376d3a7bae390d352d8c8a0412d9c9
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-09 22:04:19 +0530

    feature/404-not-found
    I create page not found error message showing page.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+import NotFound from './NotFound.jsx'
 
 const router = createBrowserRouter([
+  {
+    path:'*',
+    element: <NotFound />
+  },
   {
     path:'/',
     element: <App />
