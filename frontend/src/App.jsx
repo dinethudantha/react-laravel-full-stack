c8da7d7501a60847ff5a91008a97a5baaad3937c
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-09 22:24:02 +0530

    feature/nav-bar
    I added nav bar for front end.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-    <div>
-      This is a home page
-    </div>
-    </>
-  )
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary me-auto">
+                <Container >
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav >
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown
+                                title="Dropdown"
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/3.1">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Something
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
-export default App
+export default App;
