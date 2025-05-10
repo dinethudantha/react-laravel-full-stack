192c5444e65dd6a06d078ace55243380f7e6e6dc
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-10 22:48:53 +0530

    feature/nav-bar
    Do some changes for navbar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,34 +4,48 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "./assets/css/style.scss";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <Navbar expand="lg" className="py-4 me-auto">
-                <Container>
-                    <Navbar.Brand href="#home" className="">
-                        UrbanEdge Constructions
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse
-                        id="basic-navbar-nav"
-                        className="d-flex align-items-center justify-content-end"
-                    >
-                        <Nav className="">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">About Us</Nav.Link>
-                            <Nav.Link href="#link">Services</Nav.Link>
-                            <Nav.Link href="#link">Projects</Nav.Link>
-                            <Nav.Link href="#link">Blogs</Nav.Link>
-                            <Nav.Link href="#link">Contact Us</Nav.Link>
-                       
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <header>
+                <Navbar expand="lg" className="py-4 me-auto">
+                    <Container>
+                        <Navbar.Brand href="#home" id="logo" className="">
+                            <span>UrbanEdge</span> Constructions
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse
+                            id="basic-navbar-nav"
+                            className="d-flex align-items-center justify-content-end"
+                        >
+                            <Nav className="">
+                                <Nav.Link className="navlink" href="#home">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link className="navlink" href="#link">
+                                    About Us
+                                </Nav.Link>
+                                <Nav.Link className="navlink" href="#link">
+                                    Services
+                                </Nav.Link>
+                                <Nav.Link className="navlink" href="#link">
+                                    Projects
+                                </Nav.Link>
+                                <Nav.Link className="navlink" href="#link">
+                                    Blogs
+                                </Nav.Link>
+                                <Nav.Link className="navlink" href="#link">
+                                    Contact Us
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
         </>
     );
 }
