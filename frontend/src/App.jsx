f26e2199ba8528f80ded04ca1d74d7822f86fbfc
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-14 21:49:41 +0530

    feature/hero-section-index
    I make hero section for the website.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
 
     return (
         <>
+
+        
             <header>
                 <Navbar expand="lg" className="py-4 me-auto">
                     <Container>
@@ -46,6 +48,34 @@ function App() {
                     </Container>
                 </Navbar>
             </header>
+            <main>
+                <div
+                    id="hero"
+                    className="d-flex align-items-center justify-content-center"
+                >
+                    <div className="text-center text-white">
+                        <span style={{fontSize:"1.5rem" , color:"#ffb703"}}>Welcome Amazing Constructions</span>
+                        <h1>
+                            Crafting dreams with
+                            <br />
+                            precision and excellence.
+                        </h1>
+                        <p style={{fontSize:"1.2rem"}}>
+                            We excel at transforming visions into reality
+                            through outstanding craftsmanship and precise
+                            <br />
+                            attention to detail. With years of experience and a
+                            dedication to quality.
+                        </p>
+                        <div class="mt-4">
+                            <a class="btn btn-primary large py-3">Contact Now</a>
+                            <a class="btn btn-secondary ms-2 large py-3">
+                                View Projects
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
