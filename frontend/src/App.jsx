77ffa60a557d62f6588389ef7e9c0999ebc78281
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-05-16 15:00:31 +0530

    feature/index-new-sections
    I create about us , services & facts sections for the website.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,20 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./assets/css/style.scss";
+import aboutImg from "./assets/images/about-us.jpg";
+import service1Img from "./assets/images/construction10.jpg";
+import service2Img from "./assets/images/construction122 (2).jpg";
+import service3Img from "./assets/images/construction3.jpg";
+import service4Img from "./assets/images/construction4.jpg";
+import icon1 from "./assets/images/icon-1.svg"
+import icon2 from "./assets/images/icon-2.svg"
+import icon3 from "./assets/images/icon-3.svg"
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-
             {/* Header Section */}
 
             <header>
@@ -50,9 +57,7 @@ function App() {
                 </Navbar>
             </header>
 
-
             <main>
-
                 {/* Hero Section */}
 
                 <div
@@ -86,6 +91,229 @@ function App() {
                     </div>
                 </div>
 
+                {/* About Section */}
+
+                <div className="section-2 d-flex align-items-center justify-content-center py-5">
+                    <div className="row container">
+                        <div className="col-12 col-md-6">
+                            <img src={aboutImg} className="w-100" alt="" />
+                        </div>
+                        <div class="col-md-6 col-12">
+                            <div className="section-header">
+                                <span className="section-sub-heading">
+                                    about us
+                                </span>
+                                <h2>
+                                    Crafting structures that last a lifetime
+                                </h2>
+                                <p>
+                                    Building enduring structures requires a
+                                    comprehensive approach that combines
+                                    advanced materials, resilient design,
+                                    routine maintenance, and sustainable
+                                    practices. By drawing on historical insights
+                                    and utilizing modern technology.
+                                </p>
+                                <p>
+                                    Designing structures that stand the test of
+                                    time involves a seamless blend of
+                                    cutting-edge materials, durable design,
+                                    ongoing upkeep, and eco-friendly practices.
+                                    By combining lessons from the past with the
+                                    power of modern technology.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Services Section */}
+                <div className="section-3 py-5 bg-light d-flex flex-column align-items-center">
+                    <div className="section-header text-center">
+                        <span className="section-sub-heading">
+                            our services
+                        </span>
+                        <h2> Our construction services</h2>
+                        <p>
+                            We offer a diverse array of construction services,
+                            spanning residential, commercial, and industrial
+                            projects.
+                        </p>
+                    </div>
+                    <div className="row container-fluid mt-4">
+                        <div className="col-md-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={service1Img} alt="" />
+
+                                    <div class="services-body">
+                                        <div className="px-3 pb-4 text-white">
+                                            <div class="service-title">
+                                                <h3>Civil Construction</h3>
+                                            </div>
+                                            <div class="service-content">
+                                                <p>
+                                                    Civil construction is a core
+                                                    sector within the
+                                                    construction industry that
+                                                    focuses on the design,
+                                                    development, and maintenance
+                                                    of infrastructure that
+                                                    supports modern society.
+                                                </p>
+                                            </div>
+                                            <a
+                                                class="btn btn-primary small"
+                                                href="/service/6"
+                                            >
+                                                Read More
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={service2Img} alt="" />
+
+                                    <div class="services-body">
+                                        <div className="px-3 pb-4 text-white">
+                                            <div class="service-title">
+                                                <h3>Specialty Construction</h3>
+                                            </div>
+                                            <div class="service-content">
+                                                <p>
+                                                    Specialty construction is a
+                                                    niche sector within the
+                                                    construction industry that
+                                                    focuses on projects
+                                                    requiring specialized
+                                                    skills, materials, and
+                                                    techniques.
+                                                </p>
+                                            </div>
+                                            <a
+                                                class="btn btn-primary small"
+                                                href="/service/6"
+                                            >
+                                                Read More
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={service3Img} alt="" />
+                                    <div class="services-body">
+                                        <div className="px-3 pb-4 text-white">
+                                            <div class="service-title">
+                                                <h3>Industrial Construction</h3>
+                                            </div>
+                                            <div class="service-content">
+                                                <p>
+                                                    Industrial construction is a
+                                                    specialized sector within
+                                                    the construction industry
+                                                    that focuses on the design,
+                                                    development, and
+                                                    construction of facilities
+                                                    for industrial use.
+                                                </p>
+                                            </div>
+                                            <a
+                                                class="btn btn-primary small"
+                                                href="/service/6"
+                                            >
+                                                Read More
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={service4Img} alt="" />
+                                    <div class="services-body">
+                                        <div className="px-3 pb-4 text-white">
+                                            <div class="service-title">
+                                                <h3>Building Construction</h3>
+                                            </div>
+                                            <div class="service-content">
+                                                <p>
+                                                    Building construction is a
+                                                    broad and essential sector
+                                                    within the construction
+                                                    industry that focuses on the
+                                                    creation of structures
+                                                    designed for human occupancy
+                                                    and use.
+                                                </p>
+                                            </div>
+                                            <a
+                                                class="btn btn-primary small"
+                                                href="/service/6"
+                                            >
+                                                Read More
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Fact Section */}
+                <div className="section-4 py-5 d-flex flex-column align-items-center">
+                    <div className="section-header text-center">
+                        <span className="section-sub-heading">
+                            Why Choose Us
+                        </span>
+                        <h2> Discover our wide variety of projects.</h2>
+                        <p>
+                            Created in close partnership with our clients and
+                            collaborators, this approach merges industry
+                            expertise, <br /> decades of experience, innovation, and
+                            flexibility to consistently deliver excellence.
+                        </p>
+                    </div>
+                    <div className="container row cards-container mt-4">
+                        <div className="col-md-4 card">
+                            <div className="item shadow px-4 d-flex align-items-center">
+                                <div>
+                                    <img src={icon1} alt="" />
+                                    <h3 className="mt-2">Cutting-Edge Solutions</h3>
+                                    <p className="mt-3">Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 card">
+                            <div className="item shadow px-4 d-flex align-items-center">
+                                <div>
+                                    <img src={icon2} alt="" />
+                                    <h3 className="mt-2">Cutting-Edge Solutions</h3>
+                                    <p className="mt-3">Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 card">
+                            <div className="item shadow px-4 d-flex align-items-center">
+                                <div>
+                                    <img src={icon3} alt="" />
+                                    <h3 className="mt-2">Cutting-Edge Solutions</h3>
+                                    <p className="mt-3">Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     );
